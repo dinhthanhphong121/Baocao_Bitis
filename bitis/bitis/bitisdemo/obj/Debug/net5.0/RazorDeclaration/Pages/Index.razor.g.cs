44abb00660bb864b5c46e2fbc5b16a20e3fe9755
dinhthanhphong121 <89731713+dinhthanhphong121@users.noreply.github.com>
2021-10-23 21:35:48 +0700

    upload
    product

--- bitis/bitis/bitisdemo/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ bitis/bitis/bitisdemo/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -83,7 +83,7 @@ using bitisdemo.Shared;
 #line hidden
 #nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/")]
-    public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class Index : IndexBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
