be745d5d492307ec18694fe7f083e8ae96c1edfa
dinhthanhphong121 <89731713+dinhthanhphong121@users.noreply.github.com>
2021-10-23 19:48:19 +0700

    upload
    header
    sileder

---
+++ bitis/bitis/bitis.mode/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitis.mode
+{
+    class Category
+    {
+        public int IdDanhMuc { get; set; }
+        public string TenDanhMuc { get; set; }
+    }
+}
---
+++ bitis/bitis/bitis.mode/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bitis.mode
+{
+    public class Product
+    {
+        public int IdSp { get; set; }
+        public string MaSp { get; set; }
+        public string TenSp { get; set; }
+        public int Gia { get; set; }
+        public int SoLuong { get; set; }
+        public int IdDanhMuc { get; set; }
+        public string MoTaSp { get; set; }
+        public string ChiTietSP { get; set; }
+        public string HinhSp { get; set; }
+    }
+}
